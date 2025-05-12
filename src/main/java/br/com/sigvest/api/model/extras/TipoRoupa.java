a7fdc7cde76d088cab7cb3a31949e646aa752a84
matheeuszoka <matheuschampan@hotmail.com>
2025-05-11 22:53:55 -0300

    alter fornecedor and produto, new class marca and TipoRoupa

---
+++ src/main/java/br/com/sigvest/api/model/extras/TipoRoupa.java
@@ -0,0 +1,19 @@
+package br.com.sigvest.api.model.extras;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum TipoRoupa {
+
+
+    TIPO_CAMISETA("tipo_camiseta"),
+    TIPO_CALCA("tipo_calca"),
+    TIPO_VESTIDO("tipo_vestido"),
+    TIPO_MOLETOM("tipo_moletom"),
+    TIPO_JAQUETA("tipo_jaqueta")
+
+    private String tipoRoupa;
+
+
+
+}
